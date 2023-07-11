37422bbe5bc582701cec867244a87598fe3915f2
YevhenKolomiiets <eugene.kolomiiets@gmail.com>
2023-07-11 19:19:15 +0300

    Added registration test

---
+++ cypress/e2e/registration.cy.js
@@ -0,0 +1,46 @@
+import user from '../fixtures/user.json'
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/');
+
+    cy.log("**Opening registration form**")
+    cy.get("#customer_menu_top").click();
+    cy.get("#accountFrm button").click();
+
+    cy.log("Filling registration form");
+    cy.get("#AccountFrm_firstname").type(user.firstName);
+    cy.get("#AccountFrm_lastname").type(user.lastName);
+    cy.get("#AccountFrm_email").type(user.email);
+    cy.get("#AccountFrm_telephone").type(user.phoneNumber);
+    cy.get("#AccountFrm_fax").type(user.fax);
+    cy.get("#AccountFrm_company").type(user.company);
+    cy.get("#AccountFrm_address_1").type(user.address);
+    cy.get("#AccountFrm_city").type(user.city);
+    cy.get("#AccountFrm_country_id").select(user.country);
+    cy.get("#AccountFrm_zone_id").select(user.region);
+    cy.get("#AccountFrm_postcode").type(user.zip);
+    cy.get('#AccountFrm_loginname').type(user.loginName);
+    cy.get('#AccountFrm_password').type(user.password);
+    cy.get('#AccountFrm_confirm').type(user.password);
+    
+    cy.log("Decline newsletter and confirm Privacy policy")
+    cy.get("#AccountFrm_newsletter0").check();
+    cy.get("#AccountFrm_agree").check();
+    cy.get('.form-group [type="submit"]').click();
+
+    cy.get('.maintext').should('be.visible').and('contain', 'Your Account Has Been Created!')
+  })
+
+  it('passes', () => {
+    cy.visit('/');
+
+    cy.get('#customer_menu_top').click();
+    cy.get('#loginFrm_loginname').type(user.loginName);
+    cy.get('#loginFrm_password').type(user.password);
+    cy.get('#loginFrm button').click();
+
+    cy.get('.heading1 .subtext').should('have.text', user.firstName);
+  })
+
+})
